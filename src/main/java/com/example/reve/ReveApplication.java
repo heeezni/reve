178a5d509f7f9d94950cc31937c481fdd8b29492
heeezni <hjin3456@naver.com>
2025-07-29 16:46:34 +0900

    ✨ Feat: JPA Auditing 기능 활성화 (@EnableJpaAuditing 설정 추가)

--- src/main/java/com/example/reve/ReveApplication.java
+++ src/main/java/com/example/reve/ReveApplication.java
@@ -2,8 +2,10 @@ package com.example.reve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 엔티티의 생성/수정 시간 자동 기록 기능이 동작하도록 JPA Auditing 기능을 활성화
 public class ReveApplication {
 
   public static void main(String[] args) {
