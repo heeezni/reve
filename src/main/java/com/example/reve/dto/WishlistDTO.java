acb541fd1fa5613c1347c117d0dd9f82f3d2799d
sohee00314 <sohee00314@gmail.com>
2025-08-01 14:46:09 +0900

    🚧 WIP : 위시리스트 DTO 및 html 수정

---
+++ src/main/java/com/example/reve/dto/WishlistDTO.java
@@ -0,0 +1,12 @@
+package com.example.reve.dto;
+
+import com.example.reve.domain.Perfume;
+import lombok.Data;
+
+@Data
+public class WishlistDTO {
+    //상품 아이디
+    private Perfume perfume;
+    //찜 여부
+    private boolean wish;
+}
