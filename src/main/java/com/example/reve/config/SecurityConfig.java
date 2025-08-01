d81a769db102b263332843c369398736e8b1f7cf
sohee00314 <sohee00314@gmail.com>
2025-08-02 05:03:01 +0900

    ✨ Feat : 아이디 중복 검사 구현 완료

--- src/main/java/com/example/reve/config/SecurityConfig.java
+++ src/main/java/com/example/reve/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         "/images/**",
                         "/uploads/**",
                         "/webjars/**",
+                        "/member/check/**",
                         "/favicon.ico")
                     .permitAll()
                     .requestMatchers(
