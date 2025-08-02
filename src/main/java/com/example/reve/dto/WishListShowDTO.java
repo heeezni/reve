8346f57bd3b73f71adbac0736ee0918181f4f15f
sohee00314 <sohee00314@gmail.com>
2025-08-03 00:28:05 +0900

    🚧 WIP : 위시리스트 목록 DTO

--- src/main/java/com/example/reve/dto/WishListShowDTO.java
+++ src/main/java/com/example/reve/dto/WishListShowDTO.java
@@ -1,10 +1,23 @@
 package com.example.reve.dto;
 
-import com.example.reve.domain.Perfume;
-
 import lombok.Data;
 
 @Data
 public class WishListShowDTO {
-  private Perfume perfume;
+  // 상품 이미지
+  private String haverImageUrl;
+  // 상품명
+  private String perfumName;
+  // 할인값
+  private int discount;
+  // 가격
+  private int price;
+  // 향종류
+  private String scent;
+  // 간략 설명
+  private String desciptionTitle;
+  // 별 평균
+  private String ratingAvg;
+  // 리뷰 수
+  private int countReview;
 }
