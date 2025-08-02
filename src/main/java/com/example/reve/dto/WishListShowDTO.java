b58da29268507b646455bbccc787bbb92320f632
sohee00314 <sohee00314@gmail.com>
2025-08-02 21:38:51 +0900

    🚧 WIP : 리스트 작업중

--- src/main/java/com/example/reve/dto/WishListShowDTO.java
+++ src/main/java/com/example/reve/dto/WishListShowDTO.java
@@ -1,10 +1,10 @@
 package com.example.reve.dto;
 
 import com.example.reve.domain.Perfume;
+
 import lombok.Data;
 
 @Data
 public class WishListShowDTO {
-    private Perfume perfume;
-
+  private Perfume perfume;
 }
