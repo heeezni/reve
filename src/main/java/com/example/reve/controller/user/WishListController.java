323fe35cf19f964861859c0ac8d33cb16cccd5a5
sohee00314 <sohee00314@gmail.com>
2025-08-02 18:49:47 +0900

    🚧 WIP : 위시리스트 작업중

--- src/main/java/com/example/reve/controller/user/WishListController.java
+++ src/main/java/com/example/reve/controller/user/WishListController.java
@@ -38,6 +38,9 @@ public class WishListController {
   // 상품 목록에 있는 버튼
   @PostMapping("/wishlist/list")
   public String listWish(@RequestParam Long perfumeId, Principal principal) {
+    if (principal == null) {
+      return "redirect:/login";
+    }
     String loginId = principal.getName();
     User user =
         userRepository
