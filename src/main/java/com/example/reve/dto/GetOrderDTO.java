402fbaf1a5b30654fc1a3d6a3a90bbbcfc6887aa
sohee00314 <sohee00314@gmail.com>
2025-08-04 16:51:32 +0900

    🚧 WIP : 주문내역 준비중

---
+++ src/main/java/com/example/reve/dto/GetOrderDTO.java
@@ -0,0 +1,14 @@
+package com.example.reve.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.reve.domain.Perfume;
+import lombok.Data;
+
+@Data
+public class GetOrderDTO {
+  private String orderNumber;
+  private LocalDateTime createAt;
+  private Perfume perfume;
+  private int totalPrice;
+}
