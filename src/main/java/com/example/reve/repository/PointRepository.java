2c71b70c52bf8068673482cdf5bebedbd21d141a
jongseo0703 <jongseo0703@naver.com>
2025-08-04 12:02:02 +0900

    ✨ Feat : 포인트 가져오는 JPA 추가

--- src/main/java/com/example/reve/repository/PointRepository.java
+++ src/main/java/com/example/reve/repository/PointRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.repository.query.Param;
 
 import com.example.reve.domain.Point;
 
-public interface PointRepositroy extends JpaRepository<Point, Long> {
+public interface PointRepository extends JpaRepository<Point, Long> {
   // 유저가 가지고 있는 포인트 조회
   Optional<Point> findByUser_UserId(Long userId);
 
   @Query("SELECT p.pointAmount FROM Point p WHERE p.user.userId = :userId")
   Optional<Integer> findPointAmountByUserId(@Param("userId") Long userId);
+
+  @Query("SELECT COALESCE(SUM(p.pointAmount), 0) FROM Point p WHERE p.user.userId = :userId")
+  int getTotalPointByUserId(@Param("userId") Long userId);
 }
